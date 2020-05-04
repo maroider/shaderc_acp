3ce7555212f760e0ac6195af7f83fd7465226d20
Markus Røyset <maroider@protonmail.com>
2020-05-04 10:08:23 +0200

    Re-export bytemuck

--- shaderc_acp_macros/src/lib.rs
+++ shaderc_acp_macros/src/lib.rs
@@ -1,3 +1,6 @@
+#[doc(hidden)]
+pub use bytemuck;
+
 // TODO: Make this a procedural macro which takes the current relative path to the shader as its input
 //       Blocked on [rust-lang/rust#54725](https://github.com/rust-lang/rust/issues/54725)
 #[macro_export]
